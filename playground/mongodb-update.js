1070a77b414418a1c43c319e175027ce8d5bbc2c
Peter Öman <peter.oman@digitalroute.com>
2019-01-08 10:39:37 +0100

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID("5c3467f81e12019350e028bc")
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID("5c33747dcdb416ce243e3756")
+    }, {
+        $set: {
+            name: 'Peter'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
