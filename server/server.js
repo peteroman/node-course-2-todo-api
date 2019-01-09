f34960029d91954386007da39b3030d3664ef4a5
Peter Öman <peter.oman@digitalroute.com>
2019-01-09 14:41:58 +0100

    Add GET /users/me and auth middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -108,10 +109,14 @@ app.post('/users', (req, res) => {
     });
 });
 
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
-
 
 // var user = new User({
 //     email: 'peter@hello.com'
