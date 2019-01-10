6ccd1e165f2c1a1ff55c15b5f8b65b926814bd32
Peter Öman <peter.oman@digitalroute.com>
2019-01-10 12:46:11 +0100

    Upgrade mocha, mongodb, mongoose, supertest and expect to latest versions to get rid of warnings and deprecation messages

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
 mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
